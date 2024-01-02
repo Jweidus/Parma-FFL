52e38c0f96e6e6b6d0fa4e50fecb228269867ca4
Jweidus <155409532+Jweidus@users.noreply.github.com>
2024-01-01 23:02:21 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -268,7 +268,7 @@ export const managers = [
       "preferredContact": "Sleeper", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
    {
-      "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+      "roster": 12,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "name": "Josh Weidus",
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Columbus, OH", // (optional)
